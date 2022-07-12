5d494e3ba5281242c814d19c877a044de174ca87
FlutLab.io <>
2022-07-12 16:55:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('lolol'),
         centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
         child: Text("ll"),
