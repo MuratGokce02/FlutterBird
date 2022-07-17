9c5c64da51d1ae5752426795ced88813289c799b
FlutLab.io <>
2022-07-17 21:40:49 +0000

    Autosave

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Expanded(
+        child: Container(
+          color: Colors.blue,
+        ),
+        flex: 3,
+      ),
+      Expanded(
+        child: Container(
+          color: Colors.green,
+        ),
+        flex: 1,
+      ),
+    ]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'home.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -7,28 +8,3 @@ void main() => runApp(
         debugShowCheckedModeBanner: false,
       ),
     );
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Expanded(
-        child: Container(
-          color: Colors.blue,
-        ),
-        flex: 3,
-      ),
-      Expanded(
-        child: Container(
-          color: Colors.green,
-        ),
-        flex: 1,
-      ),
-    ]);
-  }
-}
