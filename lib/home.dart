b8a00def836464fd33c87534ef939d669207054d
FlutLab.io <>
2022-07-18 19:08:46 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
         flex: 1,
       ),
       Expanded(
-          child: Central(
+          child: Center(
         child: Text('hello'),
       ))
     ]);
