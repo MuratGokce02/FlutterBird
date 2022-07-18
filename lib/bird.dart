7f9beb348dd24b6103c53140f1508e65e7beabc3
FlutLab.io <>
2022-07-18 19:04:26 +0000

    Autosave

---
+++ lib/bird.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Bird extends StatefulWidget {
+  const Bird({Key? key}) : super(key: key);
+  _BirdState createState() => _BirdState();
+}
+
+class _BirdState extends State<Bird> {
+  @override
+  Widget build(BuildContext context) {
+    return Image();
+  }
+}
